164818f5ee95b31e881b13746131170bb00c45f6
Alamat <danjacinto24@gmail.com>
2016-09-09 14:03:33 +0800

    V3: Rotated Map; Center  Fixed Map DLSU

--- app/src/main/java/com/example/user/myapplication/MapsActivity.java
+++ app/src/main/java/com/example/user/myapplication/MapsActivity.java
@@ -19,7 +19,7 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.text.CollationElementIterator;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback{
 
     private GoogleMap mMap; // Might be null if Google Play services APK is not available.
     private GoogleApiClient mGoogleApiClient;
@@ -28,7 +28,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private CollationElementIterator mLongitudeText;
     private LocationRequest mLocationRequest;
     private GoogleApiClient mGoogleClient;
-
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -40,32 +39,44 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     }
 
     @Override
-    public void onMapReady(GoogleMap map) {
+    public void onMapReady(final GoogleMap map) {
         //set to google earth
         map.setMapType(GoogleMap.MAP_TYPE_HYBRID);
         //get current location
         map.setMyLocationEnabled(true);
-        //add marker to the buildings
-        map.addMarker(new MarkerOptions().position(new LatLng(14.565549, 120.993152)).title("Velasco"));
-        // Zoom
+        map.getUiSettings().setMyLocationButtonEnabled(false);
+        //add marker to the buildings Per Buildings (One Per QR Scan)
+        map.addMarker(new MarkerOptions().position(new LatLng(14.565448, 120.993215)).title("Lambda"));
+        map.addMarker(new MarkerOptions().position(new LatLng(14.565637, 120.992596)).title("Mu"));
+        map.addMarker(new MarkerOptions().position(new LatLng(14.566283, 120.993055)).title("Xi"));
+        map.addMarker(new MarkerOptions().position(new LatLng(14.567004, 120.992645)).title("Rho"));
+        map.addMarker(new MarkerOptions().position(new LatLng(14.566351, 120.992207)).title("Omicron"));
+
         LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
         Criteria criteria = new Criteria();
         String provider = locationManager.getBestProvider(criteria, true);
-        Location location = locationManager.getLastKnownLocation(provider);
+        final Location location = locationManager.getLastKnownLocation(provider);
 
+        // Zoom to initial position
         if (location != null) {
 
             LatLng target = new LatLng(location.getLatitude(), location.getLongitude());
+            map.addMarker(new MarkerOptions().position(new LatLng(location.getLatitude(), location.getLongitude())).title("You are here!").snippet("This is Your Starting Outdoor Position "));
+        }
+
+        //DLSU Map
+        if (location != null) {
+
+            LatLng target = new LatLng(14.565472, 120.992894);
             CameraPosition position = map.getCameraPosition();
 
             CameraPosition.Builder builder = new CameraPosition.Builder();
-            builder.zoom(20);
+            builder.zoom(17);
+            builder.tilt(0);
+            builder.bearing(245);
             builder.target(target);
-
             map.animateCamera(CameraUpdateFactory.newCameraPosition(builder.build()));
-            map.addMarker(new MarkerOptions().position(new LatLng(location.getLatitude(), location.getLongitude())).title("You are here!").snippet("This is Your Initial Position "));
         }
-
 
     }
 
